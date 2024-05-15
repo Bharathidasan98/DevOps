a2787ece18e1202016a8b81f84d5dec132e03520
Bharathidasan M <101714971+Bharathidasan98@users.noreply.github.com>
2024-05-15 18:25:33 +0530

    pushing lambda module

---
+++ aws_terraform/module/lambda/variable.tf
@@ -0,0 +1,15 @@
+variable "var_lambda_name" {
+  type = string
+  default = ""
+}
+variable "var_lambda_role" {
+  type = string
+  default = ""
+}
+variable "var_handler" {
+  type = string
+  default = ""
+}
+variable "var_lamda_rolename" {
+  type = string
+}
