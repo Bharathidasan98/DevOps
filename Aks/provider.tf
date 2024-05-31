505bb65bd064da3a51ae4e796e008c9fe04531af
Bharathidasan98 <bharathidasanm98@gmail.com>
2024-05-31 08:10:29 +0530

    feat:: aks module

--- Aks/provider.tf
+++ Aks/provider.tf
@@ -10,9 +10,7 @@ provider "azurerm" {
   features {}
   subscription_id = var.var_sub_id
   client_id       = var.var_client_id
-  #client_secret = "f2178968-407a-49ba-aee8-5ecdd56b29cd"
   client_secret = var.var_client_secret
   tenant_id     = var.var_tenant_id
   #skip_provider_registration = false
-  #whZ8Q~vYPI4NAWLRiETuVPh201AjXspMjDKcAa4A
 }
